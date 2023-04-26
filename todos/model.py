c776e0aff2dcf0e2279e20d1ae796dd89fe54195
seopbo <bsk0130@gmail.com>
2023-04-26 23:53:29 +0900

    chapter.section.subsection=2.5.0

--- todos/model.py
+++ todos/model.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "item": "Example Schema!"
+            }
+        }
