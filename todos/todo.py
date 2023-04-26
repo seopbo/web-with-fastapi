585cf41f21fec1a14b8a2d02037ec1e72014220b
seopbo <bsk0130@gmail.com>
2023-04-26 23:31:55 +0900

    Add path parameter with Path class

--- todos/todo.py
+++ todos/todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from model import Todo
 
 todo_router = APIRouter()
@@ -18,8 +18,10 @@ async def retrieve_todos() -> dict:
         "todos": todo_list
     }
 
-@todo_router.get("/todo/{todo_id}") # 경로 매개변수 추가, 경로 매개변수는 리소스를 식별하기위해 API 라우팅에 사용됨.
-async def get_single_todo(todo_id: int) -> dict:
+ # 경로 매개변수 추가, 경로 매개변수는 리소스를 식별하기위해 API 라우팅에 사용됨.
+ # Path 클래스는 라우트 함수에 있는 다른 인수와 경로 매개변수를 구분하는 역할로 사용된다. Path 클래스는 스웨거, ReDoc 등으로 OpenAPI 기반 문서를 자동 생성할 때 라우트 관련 정보를 함께 문서화하도록 도움.
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
     for todo in todo_list:
         if todo.id == todo_id:
             return {
