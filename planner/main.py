7e67b92478b7da8ae8a924fc07a9fe9bb95192af
seopbo <bsk0130@gmail.com>
2023-04-30 16:59:40 +0900

    Add event route

--- planner/main.py
+++ planner/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from routes.users import user_router
+from routes.events import event_router
 import uvicorn
 
 app = FastAPI()
 # 라우트 등록
 app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="event")
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
